89001f8f4bf30b9da27099819ac398a8112491a9
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-03-21 11:59:27 +0000

    Improved some css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,24 +24,29 @@
             </p>
         </div>
 
-        <a href="{{ route('comics.create') }}">Create post</a>
-        <div class="comic-gallery grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            @foreach ($comics as $comic)
-                <div class="comic-container">
-                    <img class="comic-image" src="{{ asset($comic->image_path) }}" />
-                    <div class="comic-info">
-                        <h1>{{ $comic->title }}</h1>
-                        <p>{{ Str::limit($comic->description, 300) }}</p>
-                    </div>
-                    <a class="continue-reading-link" href="{{ route('comics.show', $comic->id) }}">Continue Reading...</a>
-                    <div class="comic-footer">
-                        <div class="comments">
-                            <img src="{{ asset('images/comment-icon.png') }}" />
-                            <p>{{ $comic->comment_count }}</p>
+        <div class="comic-gallery-section">
+            <a class="create-post-button border border-black px-3 py-1.5 w-28 rounded-md bg-gray-200"
+                href="{{ route('comics.create') }}">Create post</a>
+            <div class="comic-gallery grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                @foreach ($comics as $comic)
+                    <a href="{{ route('comics.show', $comic->id) }}" class="w-min">
+                        <div class="comic-container">
+                            <img class="comic-image" src="{{ asset('storage/' . $comic->image_path) }}" />
+                            <div class="comic-info">
+                                <h1>{{ $comic->title }}</h1>
+                                <p>{{ Str::limit($comic->description, 300) }}</p>
+                            </div>
+                            <p class="continue-reading-link">Continue Reading...</p>
+                            <div class="comic-footer">
+                                <div class="comments">
+                                    <img src="{{ asset('images/comment-icon.png') }}" />
+                                    <p>{{ $comic->comment_count }}</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            @endforeach
+                    </a>
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
