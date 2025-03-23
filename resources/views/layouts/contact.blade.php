810419510f0c7d39185c4c7d1fc23a0a90a7a298
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-03-23 17:06:15 +0000

    Created a blank contact page

--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <p>Contact Page</p>
+    <div class="bg-blue-200">
+        <div class="w-auto bg-white h-auto py-96 text-center">
+            <h1 class="text-6xl font-semibold">This page is unavailable at the moment :/</h1>
+        </div>
     </div>
 @endsection
