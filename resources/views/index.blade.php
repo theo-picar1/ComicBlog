3710c0b9a4bfb8c7efd57debc38bc6b3d2a69052
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-03-23 13:18:46 +0000

    Completed css for home navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,6 +11,32 @@
         </div>
     </div>
 
+    <div class="home-navbar bg-gray-800 text-white font-bold text-xl flex flex-row items-center justify-between px-16 py-8">
+        <div class="flex flex-row items-center gap-8">
+            <p class="cursor-pointer hover:underline">
+                Home
+            </p>
+            /
+            <p class="cursor-pointer hover:underline">
+                About
+            </p>
+            /
+            <p class="cursor-pointer hover:underline">
+                Contact Us
+            </p>
+            /
+            <p class="cursor-pointer hover:underline">
+                View Profile
+            </p>
+
+        </div>
+
+        <div class="searchbar flex flex-row items-center gap-3">
+            <input text="text" placeholder="Search for a blog (title)..." class="text-sm p-2 w-64 border border-black">
+            <img src="{{ asset('images/search-icon.png') }}" class="w-8 h-auto cursor-pointer">
+        </div>
+    </div>
+
     <div class="home-content">
         <div class="comic-gallery-section pt-15">
             <a class="create-post-button px-3 py-4 w-max m-auto mb-10 rounded-md bg-red-600 font-bold text-white text-lg"
@@ -25,10 +51,11 @@
                                 <p>{{ Str::limit($comic->description, 300) }}</p>
                             </div>
                             <p class="continue-reading-link">Continue Reading...</p>
-                            <div class="comic-footer flex-row items-center flex gap-5 p-4 bg-gray-200 mt-5" >
+                            <div class="comic-footer flex-row items-center flex gap-5 p-4 bg-gray-200 mt-5">
                                 <div class="created-at flex-row items-center flex gap-2">
                                     <img src="{{ asset('images/time-icon.png') }}" class="w-5 h-auto" />
-                                    <p class="text-red-600 font-semibold text-xs">{{ $comic->created_at->format('d F Y') }}</p>
+                                    <p class="text-red-600 font-semibold text-xs">{{ $comic->created_at->format('d F Y') }}
+                                    </p>
                                 </div>
                                 <div class="comments flex-row items-center flex gap-2">
                                     <img src="{{ asset('images/comment-icon.png') }}" class="w-5 h-auto" />
